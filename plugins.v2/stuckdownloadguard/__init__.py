d1413928f4b64dbe0776e791363122908326a181
narratorz <narratorz@local>
2026-07-20 12:38:53 +0800

    fix(stuckdownloadguard): 1.0.2 补回 tm Client username= 参数，修复 require auth

--- plugins.v2/stuckdownloadguard/__init__.py
+++ plugins.v2/stuckdownloadguard/__init__.py
@@ -60,7 +60,7 @@ class StuckDownloadGuard(_PluginBase):
     # 插件图标
     plugin_icon = "Qbittorrent_A.png"
     # 插件版本
-    plugin_version = "1.0.1"
+    plugin_version = "1.0.2"
     # 插件作者
     plugin_author = "narrator-z"
     # 作者主页
@@ -223,6 +223,7 @@ class StuckDownloadGuard(_PluginBase):
             host=thost,
             port=tport,
             path=tpath,
+            username=cfg.get("username"),
             password=cfg.get("password"),
             protocol=protocol,
         )
